a4f0fe0491ac06f295e57498754ad765deeb7d1e
vBarbaros <victor.barbarosh@gmail.com>
2023-01-15 20:09:24 -0500

    Examples of ssh-command features

--- src/main/java/com/example/springshell/cmds/SSHLoggingCommand.java
+++ src/main/java/com/example/springshell/cmds/SSHLoggingCommand.java
@@ -3,6 +3,30 @@
  */
 package com.example.springshell.cmds;
 
+import org.springframework.shell.Availability;
+import org.springframework.shell.standard.ShellMethod;
+
+import java.util.logging.Logger;
 
 public class SSHLoggingCommand {
+    Logger log = Logger.getLogger(SSHLoggingCommand.class.getName());
+    private boolean signedIn;
+
+    @ShellMethod(value = "sign in")
+    public void signIn() {
+        this.signedIn = true;
+        log.info("Signed In!");
+    }
+
+    @ShellMethod(value = "sign out")
+    public void signOut() {
+        this.signedIn = false;
+        log.info("Signed out!");
+    }
+
+    // note the naming
+    public Availability signOutAvailability() {
+        return signedIn ?
+                Availability.available() : Availability.unavailable("Must be signed in first");
+    }
 }

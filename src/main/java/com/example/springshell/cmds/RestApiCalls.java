ca8dc14a9099dfd394cc1538bc565df499ac8c17
vBarbaros <victor.barbarosh@gmail.com>
2023-01-18 21:29:12 -0500

    Implement GET for List of CVE

--- src/main/java/com/example/springshell/cmds/RestApiCalls.java
+++ src/main/java/com/example/springshell/cmds/RestApiCalls.java
@@ -20,21 +20,37 @@ public class RestApiCalls {
     Logger log = Logger.getLogger(RestApiCalls.class.getName());
 
     @ShellMethod(key = "cpe-to", value = "Converts a CPE according to CPE 2.3 version conventions")
-    public void cpeToVersion(@ShellOption(value = "-v", defaultValue = "2.3") String toCpeVersion){
-        RestTemplate restTemplate = new RestTemplate();
+    public void cpeToVersion(@ShellOption(value = "-v", defaultValue = "2.3") String toCpeVersion) {
         String urlConvertToCpeVersion = getFinalUrlConvertToCpeVersion(toCpeVersion);
-        System.out.println(urlConvertToCpeVersion);
 
-        // Fetch JSON response as String wrapped in ResponseEntity
-        ResponseEntity<String> convertedCpe = restTemplate.getForEntity(urlConvertToCpeVersion, String.class);
-        String productsJson = convertedCpe.getBody();
+        String productsJson = callRestApi(urlConvertToCpeVersion);
         log.info(format("CPE Return: '%s'", productsJson));
+    }
+
+    @ShellMethod(key = "cve-for", value = "Outputs a list of CVEs related to the product")
+    public void cveFor(@ShellOption(value = "-l", arity = 2) String[] keys) {
+        log.info(format("CVE : '%s', Limit '%s'", keys[0], keys[1]));
+        val cpe = "cpe:2.3:o:microsoft:windows_vista:6.0:sp1:-:-:home_premium:-:-:x64";
+        // cpe:2.3:o:microsoft:windows_vista:6.0:sp1:-:-:home_premium:-:-:x64
+        String urlGetCveListForProduct = CVE_API_HOSTNAME + "cvefor/" + keys[0] + "/" + keys[1];
+
+        log.info(format(" GET CVE List : '%s'", urlGetCveListForProduct));
+        String productsJson = callRestApi(urlGetCveListForProduct);
+        log.info(format("CPE Return: '%s'", productsJson));
+    }
+
+    private static String callRestApi(String urlGetCveListForProduct) {
+        RestTemplate restTemplate = new RestTemplate();
+        // Fetch JSON response as String wrapped in ResponseEntity
+        ResponseEntity<String> convertedCpe = restTemplate.getForEntity(urlGetCveListForProduct, String.class);
+        String productsJson = convertedCpe.getBody();
+        return productsJson;
     }
 
     private String getFinalUrlConvertToCpeVersion(String toCpeVersion) {
         val cpe = "cpe:/o:microsoft:windows_vista:6.0:sp1:~-~home_premium~-~x64~-";
         String resourceUrl = CVE_API_HOSTNAME;
-        if (toCpeVersion.equals("2.2")){
+        if (toCpeVersion.equals("2.2")) {
             resourceUrl += "cpe2.2/";
         } else if (toCpeVersion.equals("2.3")) {
             resourceUrl += "cpe2.3/";
@@ -43,31 +59,11 @@ public class RestApiCalls {
         return resourceUrl;
     }
 
-    @ShellMethod(key = "cve-for", value = "Outputs a list of CVEs related to the product")
-    public void cveFor(@ShellOption(value = "-l", arity = 2) String[] keys) {
-        log.info(format("CVE : '%s', Limit '%s'", keys[0], keys[1]));
-        val cpe = "cpe:2.3:o:microsoft:windows_vista:6.0:sp1:-:-:home_premium:-:-:x64";
-        // cpe:2.3:o:microsoft:windows_vista:6.0:sp1:-:-:home_premium:-:-:x64
-        RestTemplate restTemplate = new RestTemplate();
-        String urlGetCveListForProduct = CVE_API_HOSTNAME + "cvefor/" + keys[0] + "/" + keys[1];
-
-        log.info(format(" GET CVE List : '%s'", urlGetCveListForProduct));
-        // Fetch JSON response as String wrapped in ResponseEntity
-        ResponseEntity<String> convertedCpe = restTemplate.getForEntity(urlGetCveListForProduct, String.class);
-        String productsJson = convertedCpe.getBody();
-        log.info(format("CPE Return: '%s'", productsJson));
+    @ShellMethod(value = "cve")
+    public void cve(@ShellOption(value = "--r") boolean rememberMe) {
+        log.info(format("remember me option is '%s'", rememberMe));
     }
 
-//    /**
-//     * Work with boolean params
-//     *
-//     * @param rememberMe
-//     */
-//    @ShellMethod(value = "sign in")
-//    public void sshLogin(@ShellOption(value = "--r") boolean rememberMe) {
-//        log.info(format("remember me option is '%s'", rememberMe));
-//    }
-//
 //    /**
 //     * Validate command params
 //     *
